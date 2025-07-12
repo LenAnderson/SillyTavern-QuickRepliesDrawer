11752c1a763bbd83830539654dbb2e8de8a738ef
LenAnderson <anderson.len.ja@gmail.com>
2025-07-12 12:10:23 -0400

    watch CSS

--- index.js
+++ index.js
@@ -4,11 +4,38 @@ import { hookQuickReply } from './src/helper/hookQuickReply.js';
 import { applyTweaks } from './src/helper/performance.js';
 import { QuickRepliesDrawer } from './src/QuickRepliesDrawer.js';
 
+export const NAME = new URL(import.meta.url).pathname.split('/').at(-2);
+const watchCss = async()=>{
+    if (new URL(import.meta.url).pathname.split('/').includes('reload')) return;
+    try {
+        const FilesPluginApi = (await import('../SillyTavern-FilesPluginApi/api.js')).FilesPluginApi;
+        // watch CSS for changes
+        const style = document.createElement('style');
+        document.body.append(style);
+        const path = [
+            '~',
+            'extensions',
+            NAME,
+            'style.css',
+        ].join('/');
+        const ev = await FilesPluginApi.watch(path);
+        ev.addEventListener('message', async(/**@type {boolean}*/exists)=>{
+            if (!exists) return;
+            style.innerHTML = await (await FilesPluginApi.get(path)).text();
+            document.querySelector(`#third-party_${NAME}-css`)?.remove();
+        });
+    } catch { /* empty */ }
+};
+
+/**@type {QuickRepliesDrawer} */
+export let drawer;
+
 const init = ()=>{
+    watchCss();
     applyTweaks();
     hookQuickReply();
     getCheckboxList();
-    const drawer = new QuickRepliesDrawer();
+    drawer = new QuickRepliesDrawer();
     drawer.inject();
 };
-if (!extension_settings.disabledExtensions.includes('third-party/SillyTavern-QuickRepliesDrawer')) init();
+if (!extension_settings.disabledExtensions.includes(`third-party/${NAME}`)) init();
